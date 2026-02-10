8b75d25b6c3646f3fefa8c6fa6a15f7a89786879
KRM-Real <thisismarkreal@gmail.com>
2026-02-11 00:06:24 +0800

    Refactor fibonacci_number function to initialize 'res' in the same line as 'counter' and update input value for testing

--- Practice/fibonacci.py
+++ Practice/fibonacci.py
@@ -1,7 +1,7 @@
 def fibonacci_number(n):
     
     f1 , f2 = 0 , 1
-    counter = 0
+    counter, res = 0, 0
     
     while counter < n-1:
         res = f1 + f2
@@ -13,6 +13,15 @@ def fibonacci_number(n):
 
     return res
 
-input = 4
+input = 2
 print (fibonacci_number(input))
         
+        
+    # Time : O(n)
+    # # Mem : O(n)
+    # if n == 0:return 0
+    # if n==1 : return 1
+    # fib_memo = {0:0,1:1}
+    # for i in range(2,n+1):
+    # fib_memo[i] = fib_memo[i-2]+fib_memo[i-1]
+    # return fib_memo[n]
