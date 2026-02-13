8d5488cbd1a1e4b63e74454d69a8c2b361d56f43
KRM-Real <thisismarkreal@gmail.com>
2026-02-13 23:39:03 +0800

    Enhance cholesterol distribution plot with labels and edge color

--- Machine_Learning/heart_disease_template.py
+++ Machine_Learning/heart_disease_template.py
@@ -11,7 +11,13 @@ df.describe()
 
 # %% Plot
 import matplotlib.pyplot as plt
-plt.hist(df["Cholesterol"], bins=20)
+# plt.hist(df["Cholesterol"], bins=20)
+
+plt.hist(df["Cholesterol"], bins=20, edgecolor="black")
+plt.xlabel("Cholesterol")
+plt.ylabel("Number of Patients")
+plt.title("Cholesterol Distribution (20 bins)")
+
 plt.show()
 
 # %%
