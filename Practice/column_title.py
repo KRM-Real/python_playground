9706cc60e463124af37951df6bbf65c7e51c0a51
KRM-Real <thisismarkreal@gmail.com>
2026-03-08 23:21:11 +0800

    Implement Excel Sheet Column Number

---
+++ Practice/column_title.py
@@ -0,0 +1,28 @@
+# 171. Excel Sheet Column Number
+# Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+# For example:
+# A -> 1, B -> 2, C -> 3, Z -> 26, AA -> 27, AB -> 28 
+
+
+def titleToNumber(columnTitle):
+    
+    val = [i for i in range(1, 27)]
+    letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    mapping = dict(zip(letters, val))
+    
+    i = len(columnTitle)-1
+    
+    result = 0
+    count = 0
+    
+    # print(columnTitle)
+    while i >= 0:
+        result = mapping[columnTitle[i]] * (26**count) + result
+        i-=1
+        count+=1
+        
+    return result
+
+column = "ZY"
+print(titleToNumber(column))
